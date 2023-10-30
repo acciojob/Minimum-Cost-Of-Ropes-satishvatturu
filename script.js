25605d2ef399d6c74a14fd5b29e61d1654812485
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-10-30 16:52:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
+function calculateMinCost(lengthValues) {
+
+	const inputElement = document.getElementById("rope-lengths");
+	const ropelengths = inputElement.value.split(",").map(Number);
+
+	ropelengths.sort((a,b) => a-b);
+
+	let finalRes = 0;
+	while(ropelengths.length>1)
+	{
+		const min1 = ropelengths.shift();
+		const min2 = ropelengths.shift();
+		let cost = min1+min2;
+		finalRes = finalRes + cost;
+		ropelengths.push(cost);
+		ropelengths.sort((a,b) => a-b);
+	}
+	
+	const result = document.getElementById("result");
+	result.innerText = finalRes;
 }  
